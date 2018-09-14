4a555fdb2539811b64fc02865464a42d585e9b8c
guotianyuan <gskifary@outlook.com>
2018-09-14 13:39:24 +0800

    Add `MultipartTest`

--- test/main.cpp
+++ test/main.cpp
@@ -1,20 +1,12 @@
 
 #include <gtest/gtest.h>
 
-#include <functional>
-
-
-
-
 
 int main(int argc, char* argv[])
 {
 
-
-
-
-
 	testing::InitGoogleTest(&argc, argv);
 
 	return RUN_ALL_TESTS();
+
 }
---
+++ test/multipart_test.cpp
@@ -0,0 +1,24 @@
+
+#include <gtest/gtest.h>
+
+#include <http/http.h>
+
+
+
+TEST(MultipartTests, InitTest)
+{
+
+	// from memory
+	auto memory_part = http::Part{ "text/html", (http::byte_t*)"html_data" };
+
+	// from filepath
+	auto file_part = http::Part{ "c://file.txt" };
+
+
+	auto multipart = http::Multipart{ http::Part{ "text/html", (http::byte_t*)"html_data" },http::Part{ "c://file.txt","name" },memory_part,file_part };
+
+
+
+}
+
+
